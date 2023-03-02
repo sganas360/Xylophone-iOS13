a3ec4544dab36c0a80ee24155abe6aad408b2f80
sganas360 <sganas@caesars.com>
2023-03-02 14:37:22 -0500

    Finished Module

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print(sender.currentTitle!)
+        playSound(note: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+              //Bring's sender's opacity back up to fully opaque.
+            UIView.animate(withDuration: 0.3){
+                sender.alpha = 1.0
+            }
+        }
+    }
     
-
+    func playSound(note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
-
